7a919f66b10f4bf49ebd37d7add009df773e099f
ANGELOANTU7 <angeloantu@gmail.com>
2023-05-27 16:51:03 +0530

    done

--- main.py
+++ main.py
@@ -3,11 +3,13 @@ from fastapi import FastAPI
 
 from backend.patientData import patient_route as patient
 from backend.NotesToText import router as pdftotext
+from backend.SkinAnalysis import router as skin
 app = FastAPI()
 
 
 app.include_router(patient)
 app.include_router(pdftotext)
+app.include_router(skin)
 # include other API routers as needed
 
 if __name__ == "__main__":
