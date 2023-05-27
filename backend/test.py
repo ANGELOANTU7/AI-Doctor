36c7e0cc35c21af04375418861e7484e4dbdace2
HEYRON <68512524+DarthVader-007@users.noreply.github.com>
2023-05-27 14:59:46 +0530

    test

--- backend/test.py
+++ backend/test.py
@@ -1,12 +1,26 @@
 from fastapi import File,UploadFile,APIRouter
 import chardet
+import PyPDF2
+
 
 test_route = APIRouter()
 
-def detect_encoding(data): # just to avoid any conflicts with decoding different files with different encoding
-    result = chardet.detect(data)
-    encoding = result["encoding"]
-    return encoding
+# def detect_encoding(data): # just to avoid any conflicts with decoding different files with different encoding
+#     result = chardet.detect(data)
+#     encoding = result["encoding"]
+#     return encoding
+
+
+def process_pdf(file_path: str) -> str:
+    with open(file_path, "rb") as pdf_file:
+        pdf_reader = PyPDF2.PdfReader(pdf_file)
+        # Perform operations on the PDF file, such as extracting text
+        text = ""
+        for page_num in range(len(pdf_reader.pages)):
+            page = pdf_reader.pages[page_num]
+            text += page.extract_text()
+        return text
+
 
 @test_route.get("/")
 def test():
@@ -14,9 +28,16 @@ def test():
 
 
 @test_route.post("/test-data")
-async def test_data(name : str,age : str,place : str ,file1 : UploadFile = File(...)):
-    dat1 = await file1.read()
-    enc1 = detect_encoding(dat1)
-    dat1_decoded = dat1.decode(enc1)
+async def test_data(name : str,age : str,place : str ,file : UploadFile = File(...)):
+    dat1 = await file.read()
+    file_path = f"temp/{file.filename}"
+    with open(file_path, "wb") as temp_file:
+        temp_file.write(dat1)
 
-    return {"name" : name, "age" : age, "place" : place, "encoding" : enc1, "data" : dat1_decoded}
+    # Process the PDF file
+    result = process_pdf(file_path)
+
+    # Return the result
+    {"result": result}
+
+    return {"name" : name, "age" : age, "place" : place,  "pdf_data" : result}
