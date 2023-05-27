2559632208b2b70a4cb4cfa3c1fefaab928bc507
ANGELOANTU7 <angeloantu@gmail.com>
2023-05-28 03:11:04 +0530

    sum

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from backend.patientData import patient_route as patient
 from backend.NotesToText import router as pdftotext
-#from backend.SkinAnalysis import router as skin
+from backend.SkinAnalysis import router as skin
 from backend.speech_text import router as speech2text
 from backend.Narrator import router as narrator
 from backend.eye_check import router as eye
@@ -30,8 +30,8 @@ app.include_router(narrator)
 app.include_router(aivideo)
 app.include_router(throat)
 
-#app.include_router(skin)
+app.include_router(skin)
 # include other API routers as needed
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",port=8081,host='192.168.100.185',reload=True)
+    uvicorn.run("main:app",port=8081,host='192.168.237.75',reload=True)
