4be82543c2662aa25b78bdd5b0e6249e786eee70
HEYRON <68512524+DarthVader-007@users.noreply.github.com>
2023-05-27 15:21:45 +0530

    test

--- main.py
+++ main.py
@@ -10,5 +10,4 @@ app.include_router(patient)
 # include other API routers as needed
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",port=8000,host='127.0.0.1',reload=True)
-
+    uvicorn.run("main:app",port=8000,host='192.168.39.129',reload=True)
