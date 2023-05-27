4536fcb752bc78f4ea63fc2b0ec24ec186d27aab
HEYRON <68512524+heyronjmilton@users.noreply.github.com>
2023-05-28 00:58:37 +0530

    face analyze final

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-# from backend.patientData import patient_route as patient
-# from backend.NotesToText import router as pdftotext
-# #from backend.SkinAnalysis import router as skin
-# from backend.speech_text import router as speech2text
-# from backend.Narrator import router as narrator
-# from backend.eye_check import router as eye
-# from backend.texttoAIvideo import router as aivideo
-# from backend.throat_check import router as throat
+from backend.patientData import patient_route as patient
+from backend.NotesToText import router as pdftotext
+#from backend.SkinAnalysis import router as skin
+#from backend.speech_text import router as speech2text
+from backend.Narrator import router as narrator
+from backend.eye_check import router as eye
+from backend.texttoAIvideo import router as aivideo
+from backend.throat_check import router as throat
 from backend.test_face4 import route_feed_analysis as feed_analysis
 
 app = FastAPI()
@@ -23,13 +23,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# app.include_router(patient)
-# app.include_router(pdftotext)
-# app.include_router(eye)
-# app.include_router(speech2text)
-# app.include_router(narrator)
-# app.include_router(aivideo)
-# app.include_router(throat)
+app.include_router(patient)
+app.include_router(pdftotext)
+app.include_router(eye)
+#app.include_router(speech2text)
+app.include_router(narrator)
+app.include_router(aivideo)
+app.include_router(throat)
 app.include_router(feed_analysis)
 #app.include_router(skin)
 # include other API routers as needed
