3359f2e1a232f0bbc275d5ad46ffb684bf6f8f73
HEYRON <68512524+DarthVader-007@users.noreply.github.com>
2023-05-27 14:01:02 +0530

    test

--- backend/test.py
+++ backend/test.py
@@ -3,7 +3,7 @@ import chardet
 
 test_route = APIRouter()
 
-def detect_encoding(data):
+def detect_encoding(data): # just to avoid any conflicts with decoding different files with different encoding
     result = chardet.detect(data)
     encoding = result["encoding"]
     return encoding
@@ -14,9 +14,9 @@ def test():
 
 
 @test_route.post("/test-data")
-async def test_data(file1 : UploadFile = File(...)):
+async def test_data(name : str,age : str,place : str ,file1 : UploadFile = File(...)):
     dat1 = await file1.read()
     enc1 = detect_encoding(dat1)
     dat1_decoded = dat1.decode(enc1)
 
-    return {"data" : dat1_decoded, "encoding" : enc1}
+    return {"name" : name, "age" : age, "place" : place, "encoding" : enc1, "data" : dat1_decoded}
--- main.py
+++ main.py
@@ -10,5 +10,5 @@ app.include_router(test)
 # include other API routers as needed
 
 if __name__ == "__main__":
-    uvicorn.run("main:app",port=8000,host='192.168.29.239',reload=True)
+    uvicorn.run("main:app",port=8000,host='127.0.0.1',reload=True)
 
