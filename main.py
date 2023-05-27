a49de9cd028bd952144894d86ccb8b7b2da495c3
HEYRON <68512524+DarthVader-007@users.noreply.github.com>
2023-05-27 17:44:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from backend.patientData import patient_route as patient
-from backend.NotesToText import router as pdftotext
+#from backend.NotesToText import router as pdftotext
+#from backend.SkinAnalysis import router as skin
 app = FastAPI()
 
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Replace with your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(patient)
-app.include_router(pdftotext)
+#app.include_router(pdftotext)
+#app.include_router(skin)
 # include other API routers as needed
 
 if __name__ == "__main__":
